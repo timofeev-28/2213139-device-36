9aa704bc1c4b8d71e06d99d6e9423e37f0e83fdf
Dmitrii Timofeev <28nmd@mail.ru>
2023-01-31 00:56:17 +0300

    домашка по 4 модулю

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let btnCatalog = document.querySelector('.navigation__link--js');
-let navCatalog = document.querySelector('.navigation__catalog');
+let navCatalog = document.querySelector('.navigation__list-catalog');
 
 btnCatalog.addEventListener('click', function() {
   navCatalog.classList.toggle('js-visible');
